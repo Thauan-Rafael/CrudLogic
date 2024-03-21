f89b147ed53bdffa63969a1aed0e6e3e4373e558
Thauan Rafael <thauanrafaeltrabalho@gmail.com>
2024-03-21 20:28:40 -0300

    Starting server

---
+++ api/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+require('dotenv').config();
+app.use(express.urlencoded({ extended: true}));
+app.use(cors())
+
+app.listen(3000, () => {
+    console.log('App running on port 3000');
+})
+app.post('/', (req,res) => {
+    const { cardTitle, cardText, cardColor } = req.body;
+    pool.query('INSERT INTO cards (title,text,color) VALUES (?,?,?)',[cardTitle,cardText,cardColor], (error) => {
+        if (error) {
+          console.error('Error executing query:', error);
+          return;
+        }
+        console.log('Data saved with success');
+        res.redirect('http://localhost:5173')
+      });
+})
+app.get('/checkCards', (req,res) => {
+    pool.query('SELECT * FROM cards', (error, results) => {
+        if (error) {console.error('Error checking row in database:', error);
+            return;
+        }
+        if (results.length > 0) {res.json({ exists: true, rows: results });} 
+        else {res.json({ exists: false });}
+    });
+})
+app.get('/updateCard/:id/:title/:text/:color', (req,res) => {
+    cardTitle = req.params.title;
+    cardText = req.params.text;
+    cardColor = req.params.color;
+    pool.query('UPDATE cards SET title=?, text=?, color=? WHERE id=?',[cardTitle, cardText, cardColor, req.params.id],(error, results) => {
+        if(error){console.error('Error in updating card:', error)}
+        else{res.json()}
+    })
+})
+app.get('/deleteCard/:id', (req,res) => {
+    pool.query(`DELETE FROM cards WHERE id=${req.params.id}`,(error,results) =>{
+        if(error){console.error('Error in deleting card:', error)}
+        else{res.json()}
+    })
+})
+const pool = mysql.createPool({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+  });
+  
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 function App() {
   const [cards, setCards] = React.useState([]);
-  function createCard() {
-    let title = document.querySelector('#cardTitle').value;
-    let text = document.querySelector('#cardText').value;
-    let color = document.querySelector('#cardColor').value;
-    if(cards.length > 2){
-      alert('The maximum of cards is 3');
+  function createCard(event){
+    event.preventDefault();
+    if(cards.length >=3){
+      alert('Maximum 3');
+      return
     }
-    else{
-    setCards([...cards, <Card title={title} text={text} color={color}></Card>]);
+    else if(cards.length < 3){
+      document.querySelector('#entries').submit();
     }
   }
+  React.useEffect(() => {
+    fetch('http://localhost:3000/checkCards')
+        .then(response => response.json())
+        .then(data => {
+            if (data.exists) {
+              let rowCards = data.rows;
+              setCards(cards => {
+              return rowCards.map(row => {
+                let { title: dbTitle, text: dbText, color: dbColor } = row;
+                 return <Card key={row.id} id={row.id} title={dbTitle} text={dbText} color={dbColor}></Card>;
+              })
+            })
+        }})
+        .catch(error => {
+            console.error('Error in connection:', error);
+        });
+  },[]);
   return (
     <>
     <div id="pageTitle">
       <h1>CRUD LOGIC</h1>
     </div>
-    <div id="entries" className="container">
+    <form id="entries" className="container" method="post" action="http://localhost:3000">
       <label htmlFor='cardTitle'>Title</label>
       <select id="cardTitle" name="cardTitle">
         <option value='Admin'>Admin</option>
@@ -40,26 +56,57 @@ function App() {
         <option value='green'>Green</option>
       </select>
       <button className="btn btn-success" onClick={createCard}>Create</button>
-    </div>
+    </form>
     <div id="cardsSection" className="container-fluid">
     {cards}
     </div>
     </>
   )
   function Card(props) {
+    function UpdateCard(){
+      let cardTitle = document.querySelector('#cardTitle').value;
+      let cardText = document.querySelector('#cardText').value;
+      let cardColor = document.querySelector('#cardColor').value;
+      console.log(cardTitle)
+      fetch(`http://localhost:3000/updateCard/${props.id}/${cardTitle}/${cardText}/${cardColor}`)
+      .then(response => {
+        if(response.ok){
+          location.reload();
+        }
+        else{
+          console.log('Failed to update card');
+        }
+      })
+      .catch(error => {
+        console.error('Error updating card:', error);
+      }); 
+    }
+    function DeleteCard(){
+      fetch(`http://localhost:3000/deleteCard/${props.id}`)
+      .then(response => {
+        console.log(response.ok)
+        if(response.ok){
+          location.reload();
+        }
+        else{
+          console.log('Failed to delete card');
+        }
+      })
+      .catch(error => {
+        console.error('Error deleting card:', error);
+      });
+    }
     return(
       <div id="crudCard" className="container" style={{backgroundColor: props.color}}>
         <h3>{props.title}</h3>
         <h5>{props.text}</h5>
         <div id="cardButtons">
-          <button className="cardBtn btn btn-warning">Update</button>
-          <button className="cardBtn btn btn-danger">Delete</button>
+          <button className="cardBtn btn btn-warning" onClick={UpdateCard}>Update</button>
+          <button className="cardBtn btn btn-danger" onClick={DeleteCard}>Delete</button>
         </div>
         
       </div>
     )
   }
 }
-
-
 export default App
